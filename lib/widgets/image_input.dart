c2b8e5f081403402e86c822b14faafa6ab276a88
Veng-Eang <oengvengeang@gmail.com>
2024-03-18 21:33:47 -0500

    add image picker

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,6 +11,19 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedImage;
+  void _tackPicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,7 +36,7 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       alignment: Alignment.center,
       child: TextButton.icon(
-          onPressed: () {},
+          onPressed: _tackPicture,
           icon: const Icon(Icons.camera_alt),
           label: const Text('Take Picture')),
     );
