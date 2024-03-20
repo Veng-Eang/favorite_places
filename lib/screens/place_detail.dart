e72934b53d034be61269f9c3da37c98851a29096
Veng-Eang <oengvengeang@gmail.com>
2024-03-20 02:40:56 -0500

    update place detail

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -20,50 +20,57 @@ class PlaceDetailScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(place.title),
         ),
-        body: Stack(
-          children: [
-            Image.file(
-              place.image,
-              fit: BoxFit.cover,
-              width: double.infinity,
-            ),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 70,
-                      backgroundImage: NetworkImage(locationImage),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 16,
+        body: Container(
+          alignment: Alignment.center,
+          child: Stack(
+            children: [
+              Image.file(
+                place.image,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+              Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 70,
+                        backgroundImage: NetworkImage(locationImage),
                       ),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.transparent,
-                            Colors.black54,
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
+                      Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 16,
                         ),
-                      ),
-                      child: Text(
-                        place.location.address,
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                              color: Theme.of(context).colorScheme.onBackground,
-                            ),
-                      ),
-                    )
-                  ],
-                ))
-          ],
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Colors.black54,
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                        child: Text(
+                          place.location.address,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(
+                                color:
+                                    Theme.of(context).colorScheme.onBackground,
+                              ),
+                        ),
+                      )
+                    ],
+                  ))
+            ],
+          ),
         ));
   }
 }
